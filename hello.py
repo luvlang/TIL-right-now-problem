8f7f4bc1325bced824d0b0d70230d49c7569f039
hello-kitsch <jylucia2002@naver.com>
2024-07-23 13:10:58 +0900

    ml경진대회 공부

--- hello.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-titanic = sns.load_dataset('titanic')
-print(titanic.head())
-# sns.histplot(data=titanic, x='age')
-# plt.show()
